54fd3a7d3b6b7cf7483315c4b4eb3966cfd57d93
DavidEdsonDoNascimento <david.contato.tec@gmail.com>
2024-07-25 17:37:04 -0300

    adjust keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,6 +3,10 @@ local opts = { noremap = true, silent = true }
 
 keymap.set("n", "x", '"_x')
 
+-- return to Normal Mode
+
+keymap.set("i", "jk", "<esc>l", { desc = "Return to normal mode" })
+
 -- Increment/decrement
 keymap.set("n", "+", "<C-a>")
 keymap.set("n", "-", "<C-x>")
@@ -16,18 +20,25 @@ keymap.set("n", "<Leader>q", ":quit<Return>", opts)
 keymap.set("n", "<Leader>Q", ":qa<Return>", opts)
 
 -- File explorer with NvimTree
-keymap.set("n", "<Leader>f", ":NvimTreeFindFile<Return>", opts)
 keymap.set("n", "<Leader>t", ":NvimTreeToggle<Return>", opts)
 
 -- Tabs
-keymap.set("n", "te", ":tabedit")
+keymap.set("n", "te", ":tabedit<Return>")
 keymap.set("n", "<tab>", ":tabnext<Return>", opts)
 keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
-keymap.set("n", "tw", ":tabclose<Return>", opts)
+keymap.set("n", "tq", ":tabclose<Return>", opts)
 
 -- Split window
 keymap.set("n", "ss", ":split<Return>", opts)
 keymap.set("n", "sv", ":vsplit<Return>", opts)
+
+-- New split vertical with nvim-config
+keymap.set("n", "ke", ":split ~/.config/nvim/lua/config/keymaps.lua<Return>")
+
+-- Add empty line
+keymap.set("n", "op", "o<esc>k")
+keymap.set("n", "oi", "O<esc>j")
+keymap.set("n", "oo", "A<Return>")
 
 -- Move window
 keymap.set("n", "sh", "<C-w>h")
